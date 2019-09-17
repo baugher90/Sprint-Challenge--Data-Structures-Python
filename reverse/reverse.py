79428ad724e0c56d90d7d330c3f482efae85282c
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-09-16 21:33:25 -0400

    basic reversal

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -24,7 +24,7 @@ class LinkedList:
     node = Node(value)
     if self.head is not None:
       node.set_next(self.head)
-    
+
     self.head = node
 
   def contains(self, value):
@@ -32,7 +32,7 @@ class LinkedList:
       return False
     # get a reference to the node we're currently at; update this as we traverse the list
     current = self.head
-    # check to see if we're at a valid node 
+    # check to see if we're at a valid node
     while current:
       # return True if the current value we're looking at matches our target value
       if current.get_value() == value:
@@ -43,5 +43,10 @@ class LinkedList:
     return False
 
   def reverse_list(self):
-    # TO BE COMPLETED
-    pass
+    last_node = None
+    current = self.head
+    while(current is not None):
+      next_node = current.get_next()
+      current.set_next(last_node)
+      last_node = current
+      current = next_node
