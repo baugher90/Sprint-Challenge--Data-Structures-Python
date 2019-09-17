9745c1148e3104dfeb9e0571719b479b724402cc
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-09-16 22:04:11 -0400

    faster duplicate search

--- names/names.py
+++ names/names.py
@@ -11,10 +11,17 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# runtime: 8.236852407455444 seconds
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+# runtime: 1.1329784393310547 seconds
+for i in names_1:
+    if i in names_2:
+        duplicates.append(i)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
